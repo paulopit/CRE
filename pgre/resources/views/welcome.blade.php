64de6ed3bc77f7c156a7beaa723ddbbbd8e9febb
SalterFernandes <salter.sfernandes@gmail.com>
2022-09-27 22:03:59 +0100

    texto alterado na pagina inicial

--- pgre/resources/views/welcome.blade.php
+++ pgre/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel teste sergio
                 </div>
 
                 <div class="links">
