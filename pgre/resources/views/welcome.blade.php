ad6e30ae8787a53786eb6b161a2e628f58c3b657
Deivid Vieira <brotherdeivid@gmail.com>
2022-09-27 21:29:34 +0100

    adicionado pasta de components na pasta views e alterado uma palavra no link do ficheiro welcome.blade

--- pgre/resources/views/welcome.blade.php
+++ pgre/resources/views/welcome.blade.php
@@ -86,8 +86,8 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://laracasts.com">Teste</a>
+                    <a href="https://laravel-news.com">News</a>s
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
