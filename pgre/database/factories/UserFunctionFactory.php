7c098477010e7fade38f572ac3663f81aa87ed1d
Paulo <Paulojspinto@hotmail.com>
2022-10-10 22:55:17 +0100

    Migrations Users, user_type and functions. Middleware admin, technician

---
+++ pgre/database/factories/UserFunctionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\User_function;
+use Faker\Generator as Faker;
+
+$factory->define(User_function::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ pgre/database/factories/UserTypeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\User_type;
+use Faker\Generator as Faker;
+
+$factory->define(User_type::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
