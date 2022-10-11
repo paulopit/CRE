7bc075a1641333b1b037396dda0e5318fff33ad0
Sérgio Fernandes <sfernandes@atinformatica.pt>
2022-10-11 15:57:38 +0100

    Adicionado formulário para editar dados da conta de utilizador. (em falta mensagens de erro e sucesso nas validações)

---
+++ pgre/app/Http/Controllers/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\User_function;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $userinfo = User::find(Auth::user()->id);
+        $user_functions = User_function::all();
+        return view('user.show', ['userinfo' => $userinfo, 'user_functions' =>$user_functions]);
+    }
+
+    public function show(User $user)
+    {
+
+        return view('user.show');
+    }
+    public function update(Request $request, User $user)
+    {
+        $this->validate($request, [
+            'user_name' => 'required'
+        ]);
+
+        $user_info = User::find($user->id);
+        $user_info->name = $request->user_name;
+        $user_info->birth_date = $request->user_birth_date;
+        $user_info->user_function_id = $request->user_function;
+
+        if(isset($request->user_password) && isset($request->user_password2)){
+            if($request->user_password == $request->user_password2){
+                $user_info->password = Hash::make($request->user_password);
+            }else{
+
+            }
+        }
+        $user_info->save();
+        return redirect('user/settings')->with('status','Dados pessoais alterados com sucesso!');
+    }
+}
