6860f7bb7c65009323e217b75c662059096bdc40
Paulo <Paulojspinto@hotmail.com>
2022-10-17 20:54:11 +0100

    Seeds equipment types

--- pgre/database/seeds/EquipmentTypeSeeder.php
+++ pgre/database/seeds/EquipmentTypeSeeder.php
@@ -11,6 +11,82 @@ class EquipmentTypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \DB::table('equipment_types')->insert([
+            [
+                'type' => 'Bateria',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Cabo USB',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Câmara',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Carregador com cabo',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Cartão de memória',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Estabilizador',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Fita',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Foco Projetor',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Micro Ambiente',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Mochila de costas',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Mochila de mão',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Objetiva',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Saco de mão',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Saco de tripé',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'type' => 'Tripé',
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+        ]);
     }
 }
