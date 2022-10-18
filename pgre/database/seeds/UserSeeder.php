0e50a6b94192877c3873958cf8f0f323940f9e90
Paulo <Paulojspinto@hotmail.com>
2022-10-18 17:58:55 +0100

    Seeds admin, tecnico, aluno

--- pgre/database/seeds/UserSeeder.php
+++ pgre/database/seeds/UserSeeder.php
@@ -12,7 +12,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('users')->insert(
+        \DB::table('users')->insert([
             [
                 'name' => 'Administrador',
                 'phone' => null,
@@ -23,6 +23,29 @@ class UserSeeder extends Seeder
                 'password' => Hash::make('admin'),
                 'created_at' => now(),
                 'updated_at' => now()
-            ]);
+            ],
+            [
+                'name' => 'Tecnico',
+                'phone' => null,
+                'birth_date' => carbon::createFromDate(1900, 1, 1),
+                'user_function_id' => 2,
+                'user_type_id' => 2,
+                'email' => 'tecnico@admin.com',
+                'password' => Hash::make('tecnico'),
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Aluno',
+                'phone' => null,
+                'birth_date' => carbon::createFromDate(1900, 1, 1),
+                'user_function_id' => 4,
+                'user_type_id' => 3,
+                'email' => 'aluno@admin.com',
+                'password' => Hash::make('aluno'),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+        ]);
     }
 }
