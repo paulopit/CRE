5f3db5182ed77653c9e959cff62f2f574d5418f6
Sérgio Fernandes <sfernandes@atinformatica.pt>
2022-10-24 17:21:02 +0100

    adicionada listagem de equipamentos no popup de entrega de requisição.

--- pgre/resources/views/requisition/management/details/components/deliver.blade.php
+++ pgre/resources/views/requisition/management/details/components/deliver.blade.php
@@ -14,16 +14,41 @@
                         <x-adminlte-input name="req_id" label="" placeholder="" value="{{$req_details->id}}" fgroup-class="col-md-12">
                         </x-adminlte-input>
                     </div>
+                    <x-adminlte-input name="req_days" label="Nº Dias" placeholder="Nº Dias" value="{{$req_details->request_days}}" fgroup-class="col-md-12">
+                        <x-slot name="prependSlot">
+                            <div class="input-group-text">
+                                <i class="fas fa-hashtag text-lightblue"></i>
+                            </div>
+                        </x-slot>
+                    </x-adminlte-input>
+                    <x-adminlte-input name="req_pickup_name" label="Requerente" placeholder="Requerente" value="" fgroup-class="col-md-12">
+                        <x-slot name="prependSlot">
+                            <div class="input-group-text">
+                                <i class="fas fa-user text-lightblue"></i>
+                            </div>
+                        </x-slot>
+                    </x-adminlte-input>
 
 
-
-
-
-                    <h5 class="modal-title">Tem a certeza que pretende aprovar a requisição?</h5>
+                    @if(count($req_details->lines) > 0)
+                        <hr/>
+                        <label>Estado equipamentos</label>
+                        <div class="row">
+                            @foreach($req_details->lines as $equip)
+                                <div class="col-lg-12">
+                                    @if($equip->equipment->status_ok)
+                                        <x-adminlte-input-switch data-on-text="OK" data-off-text="NOK" label="{{$equip->equipment->reference}} - {{$equip->equipment->description}}" name="equipment_status_{{$equip->id}}" data-on-color="lightblue" data-off-color="secondary" checked/>
+                                    @else
+                                        <x-adminlte-input-switch data-on-text="OK" data-off-text="NOK" label="{{$equip->equipment->reference}} - {{$equip->equipment->description}}" name="equipment_status_{{$equip->id}}" data-on-color="lightblue" data-off-color="secondary"/>
+                                    @endif
+                                </div>
+                            @endforeach
+                        </div>
+                    @endif
                 </div>
                 <div class="modal-footer">
-                    <x-adminlte-button class="btn-flat" data-dismiss="modal" label="Não" theme="info" icon="fas fa-lg fa-times"/>
-                    <x-adminlte-button class="btn-flat" type="submit" label="Sim" theme="secondary" icon="fas fa-lg fa-check"/>
+                    <x-adminlte-button class="btn-flat" data-dismiss="modal" label="Cancelar" theme="info" icon="fas fa-lg fa-times"/>
+                    <x-adminlte-button class="btn-flat" type="submit" label="Submeter" theme="secondary" icon="fas fa-lg fa-check"/>
                 </div>
             </form>
         </div>
